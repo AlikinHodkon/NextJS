0f5283b9740af9a13c59c2c49443bacf85a5f0fc
Aleksandr Prikhodko <aleksandr.prikhodko@jetruby.com>
2025-08-07 01:56:07 +0300

    Create First Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h2>Welcome to the Game</h2>
+    <div className="text-center">
+      <h2 className="text-2xl">Welcome to the Game</h2>
+      <p>Fuck you, Igor!!!</p>
     </div>
   );
 }
